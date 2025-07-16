682f1e83dd61dac5c2f8e8b068c0b4319c1890e9
Qyriad <qyriad@qyriad.me>
2025-07-16 14:26:00 +0200

    WIP: feat(stdlib): init word module
    Needs docs and more tests

---
+++ stdlib/tests/word/mod.rs
@@ -0,0 +1,34 @@
+use core::iter;
+
+use miden_core::{Felt, Word};
+use miden_utils_testing::{ONE, StackInputs, ZERO, felt_slice_to_ints, rand::seeded_word};
+
+#[test]
+fn test_word_eq() {
+    const SOURCE: &str = "
+        use.std::word
+
+        begin
+            exec.word::eq
+        end
+    ";
+
+    let mut seed = 0xf20;
+
+    for _ in 0..256 {
+        let lhs: Word = seeded_word(&mut seed);
+        let rhs: Word = seeded_word(&mut seed);
+
+        let operand_stack = iter::empty()
+            .chain(rhs.iter().rev())
+            .chain(lhs.iter().rev())
+            .map(Felt::as_int)
+            .collect::<Vec<_>>();
+
+        let result_felt = if lhs == rhs { ONE } else { ZERO };
+
+        let result = vec![result_felt.as_int()];
+
+        build_debug_test!(SOURCE, &operand_stack).expect_stack(&result);
+    }
+}
